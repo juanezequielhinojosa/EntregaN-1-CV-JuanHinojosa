d4950643e550fd705c0d246239066fb8d08ecf95
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-01-26 15:21:22 -0300

    agregando archivos

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* Async - Await */
+async function obtenerDatos(direccion) {
+    try {
+      let response = await fetch(direccion);
+      let apiResponse = await response.json();
+      console.log(apiResponse);
+      let nombre = apiResponse.results[0].name.first;
+      let apellido = apiResponse.results[0].name.last;
+      let nombreApellido = nombre + " " + apellido;
+      let email = apiResponse.results[0].email;
+      let cel = apiResponse.results[0].phone;
+      let ciudad = apiResponse.results[0].location.city;
+      let pais = apiResponse.results[0].location.country;
+      let edad = apiResponse.results[0].dob.age;
+      let imgUrl = apiResponse.results[0].picture.large;
+     
+      document.getElementById("nombreApellido").innerHTML = nombreApellido;
+      document.getElementById("photo").innerHTML = `<img src="${imgUrl}" alt="Foto de una persona">`;
+      document.getElementById("nombre").innerHTML = " " + nombre;
+      document.getElementById("email").innerHTML = " " + email;
+      document.getElementById("celular").innerHTML = " " + cel;
+      document.getElementById("direccion").innerHTML = " " + ciudad + " " + pais;
+    } catch {
+      console.log("Algo paso, no se pudo resolver...");
+    }
+  }
+  
+  const direccion = "https://randomuser.me/api/?results=1";
+  document.addEventListener("DOMContentLoaded", obtenerDatos(direccion));
+  
